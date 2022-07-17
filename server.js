bd7a6368509092d17c7a3d411abd7e6fa8f52b08
vk2215 <vaishnavikale22301@gmail.com>
2022-07-17 15:52:15 +0530

    'addcors'

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
+const cors = require('cors');
+// Cors 
+const corsOptions = {
+  origin: process.env.ALLOWED_CLIENTS.split(',')
+  // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
+}
 
+// Default configuration looks like
+// {
+//     "origin": "*",
+//     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     "preflightContinue": false,
+//     "optionsSuccessStatus": 204
+//   }
+
+app.use(cors(corsOptions))
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
@@ -11,14 +26,14 @@ connectDB();
 
 app.use(express.json());
 
-//Template engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
 
-//Routes
+// Routes 
 app.use('/api/files', require('./routes/files'));
 app.use('/files', require('./routes/show'));
 app.use('/files/download', require('./routes/download'));
+
 
 app.listen(PORT, console.log(`Listening on port ${PORT}.`));
 
