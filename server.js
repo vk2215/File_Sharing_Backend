0d14ffa5cf1d2f2131758d038cc1ef9c9c26ba90
vk2215 <vaishnavikale22301@gmail.com>
2022-07-17 19:50:19 +0530

    'addscript.js'

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
-// Cors 
+// Cors
+
 const corsOptions = {
   origin: process.env.ALLOWED_CLIENTS.split(',')
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
+
 
 // Default configuration looks like
 // {
