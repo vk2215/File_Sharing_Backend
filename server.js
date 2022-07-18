4aec53212b61b88ab4027370c0a9668e0e30071c
vk2215 <vaishnavikale22301@gmail.com>
2022-07-18 19:58:00 +0530

    'addcors'

--- server.js
+++ server.js
@@ -5,12 +5,13 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
 // Cors
-
+/*
 const corsOptions = {
   origin: (process.env.ALLOWED_CLIENTS)?.split(",")
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
-
+*/
+app.use(cors());
 
 
 
@@ -22,7 +23,7 @@ const corsOptions = {
 //     "optionsSuccessStatus": 204
 //   }
 //(corsOptions)
-app.use(cors(corsOptions));
+//app.use(cors(corsOptions));
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
