1698f3a894f9f890b5ea575cc273ca0d646d47f9
vk2215 <vaishnavikale22301@gmail.com>
2022-07-18 22:01:22 +0530

    'addfile'

--- routes/show.js
+++ routes/show.js
@@ -5,19 +5,12 @@ router.get('/:uuid', async (req, res) => {
     try {
         const file = await File.findOne({ uuid: req.params.uuid });
         // Link expired
-        if (!file) {
-            return res.render('download', { error: 'Link has been expired.' });
-        }
-        return res.render('download', { 
-            uuid: file.uuid, 
-            fileName: file.filename, 
-            fileSize: file.size, 
-            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
-            
-            //http://localhost:3000/files/download/getsb-3256ndh-jdjijdj
-        });
-    } catch (err) {
-        return res.render('download', { error: 'Something went wrong.' });
+        if(!file) {
+            return res.render('download', { error: 'Link has been expired.'});
+        } 
+        return res.render('download', { uuid: file.uuid, fileName: file.filename, fileSize: file.size, downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}` });
+    } catch(err) {
+        return res.render('download', { error: 'Something went wrong.'});
     }
 });
 
