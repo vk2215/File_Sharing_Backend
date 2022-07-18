264bc714c250262d326a2c432c7f746ec41282b9
vk2215 <vaishnavikale22301@gmail.com>
2022-07-18 17:10:24 +0530

    'fileadd'

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const cors = require('cors');
 // Cors
-
+/*
 const corsOptions = {
   origin: (process.env.ALLOWED_CLIENTS)?.split(",")
   // ['http://localhost:3000', 'http://localhost:5000', 'http://localhost:3300']
 }
+*/
 
 
 
@@ -20,8 +21,8 @@ const corsOptions = {
 //     "preflightContinue": false,
 //     "optionsSuccessStatus": 204
 //   }
-
-app.use(cors(corsOptions))
+//(corsOptions)
+app.use(cors)
 app.use(express.static('public'));
 
 const connectDB = require('./config/db');
