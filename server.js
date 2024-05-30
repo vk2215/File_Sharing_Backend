0943a168421726a5ce8cfd977b444c968ec8051c
Vaishnavi Bharat Kale <100363871+vk2215@users.noreply.github.com>
2024-05-31 01:19:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ connectDB();
 app.use(express.json());
 
 //app.set('views', path.join(__dirname, '/views'));
-app.use("/", express.static(path.join(__dirname, '/views'))))
+app.use("/", express.static(path.join(__dirname, '/views')))
 app.set('view engine', 'ejs');
 
 // Routes 
